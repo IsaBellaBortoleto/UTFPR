1cea346821ce4f9f82af5e9895c8acac5913891d
Isabela Bella Bortoleto <126192551+IsaBellaBortoleto@users.noreply.github.com>
2025-03-07 19:32:29 -0300

    ...

--- 3 Periodo/ED2/Exercicios/aula4_tries/main.c
+++ 3 Periodo/ED2/Exercicios/aula4_tries/main.c
@@ -1,18 +1,37 @@
 #include "trie.h"
 
-int main () {
+int main()
+{
 
-  Trie *root = create_node ('\0');
+    Trie* root = create_node('\0');
 
-  root = insert (root, "maria");
-  
-  printf("\n\n\n\n\n");
-  //print (root, 0);
+    root = insert(root, "maria");
+    root = insert(root, "roupa");
+    root = insert(root, "roupas");
+    root = insert(root, "rato");
+    root = insert(root, "casa");
+    root = insert(root, "castor");
+    root = insert(root, "mesa");
+    root = insert(root, "morro");
+    root = insert(root, "gorro");
+    root = insert(root, "galho");
 
-  search (root, "maria");
+    printf("\n\n\n\n\n");
+    print(root, 0);
 
-  delete (root, "mar");
-  //print (root, 0);
+    search(root, "maria");
+    search(root, "mes");
+    search(root, "tatu");
+    search(root, "galhos");
+    search(root, "casa");
 
-  return 0;
+
+    delete (root, "mar");
+    delete (root, "mes");
+    delete (root, "tatu");
+    delete (root, "galhos");
+    delete (root, "roupa");
+    print(root, 0);
+
+    return 0;
 }
--- 3 Periodo/ED2/Exercicios/aula4_tries/trie.c
+++ 3 Periodo/ED2/Exercicios/aula4_tries/trie.c
@@ -1,111 +1,119 @@
 #include "trie.h"
 
 /*  */
-Trie *create_node (char data) {
-  Trie *node = (Trie *)malloc(sizeof(Trie));
-  node->data = data;
-  node->end = FALSE;
-  node->nchild = 0;
-  int i;
-  //size is the alphabet size 
-  for (i = 0; i < size; i++) {
-    node->keys[i] = NULL;	   
-  }
-  return node;   
+Trie* create_node(char data) {
+	Trie* node = (Trie*)malloc(sizeof(Trie));
+	node->data = data;
+	node->end = FALSE;
+	node->nchild = 0;
+	int i;
+	//size is the alphabet size 
+	for (i = 0; i < size; i++) {
+		node->keys[i] = NULL;
+	}
+	return node;
 }
 
 /* */
-void free_node (Trie *node) {
-  int i;
-  for (i = 0; i < size; i++) {
-    if (node->keys[i] != NULL) {
-      free_node (node->keys[i]);	    
-    }
-  }	  
-  free (node);
+void free_node(Trie* node) {
+	int i;
+	for (i = 0; i < size; i++) {
+		if (node->keys[i] != NULL) {
+			free_node(node->keys[i]);
+		}
+	}
+	free(node);
 }
 
 /* */
-Trie *insert (Trie *root, char *word) {
-  //printf("\nImplementar a inserção\n");
-  if(search_auxiliary(root,word)){
-	printf("Error: already inserted\n");
-	return root;
-  }
-  Trie*t = root;
+Trie* insert(Trie* root, char* word) {
+	//printf("\nImplementar a inserção\n");
+	if (search_auxiliary(root, word)) {
+		printf("Error: already inserted\n");
+		return root;
+	}
+	Trie* t = root;
 
-  for(int i =0;word[i]!='\0';i++)
-  {
-	int index = word[i]-'a';
-	if(t->keys[index]==NULL)
-		t->keys[index]=create_node(word[i]);
-	t->nchild+=1;
-	t=t->keys[index];
-  }
-  t->end=TRUE;
-  return root;  	
-}
-
-/* */
-int search_auxiliary (Trie *root, char *word) {
-  //printf("\nImplementar a busca\n");
-	Trie* t=root;
-	for(int i=0;word[i]!='\0';i++)
+	for (int i = 0; word[i] != '\0'; i++)
 	{
-		int index=word[i]-'a';
-		if(t->keys[index]==NULL)
+		int index = word[i] - 'a';
+		if (t->keys[index] == NULL)
+			t->keys[index] = create_node(word[i]);
+		t->nchild += 1;
+		t = t->keys[index];
+	}
+	t->end = TRUE;
+	return root;
+}
+
+/* */
+int search_auxiliary(Trie* root, char* word) {
+	//printf("\nImplementar a busca\n");
+	Trie* t = root;
+	for (int i = 0; word[i] != '\0'; i++)
+	{
+		int index = word[i] - 'a';
+		if (t->keys[index] == NULL)
 			return FALSE;
 		else
-			t=t->keys[index];
+			t = t->keys[index];
 
 	}
-	if(t->end)
+	if (t->end)
 		return TRUE;
 	else
 		return FALSE;
 }
 
-void search (Trie *root, char *word) {
-  if (search_auxiliary (root, word))
-    printf("Word %s found!\n", word);
-  else  
-    printf("Error: word %s not found!\n", word);
+void search(Trie* root, char* word) {
+	if (search_auxiliary(root, word))
+		printf("Word %s found!\n", word);
+	else
+		printf("Error: word %s not found!\n", word);
 }
 
 /* */
-void print (Trie *root, int level) {
-  if (root != NULL) {
-    int i;
-    if (root->data == '\0')
-      printf ("(null)");	    
-    for (i = 0; i < level; i++) {
-      printf ("| ");
-    }	    
-    printf ("%c (%d - %d)\n", root->data, root->end, root->nchild);
-    for (i = 0; i < size; i++) {
-      print (root->keys[i], level+1);      
-    }
-  }	  
+void print(Trie* root, int level) {
+	if (root != NULL) {
+		int i;
+		if (root->data == '\0')
+			printf("(null)");
+		for (i = 0; i < level; i++) {
+			printf("| ");
+		}
+		printf("%c (%d - %d)\n", root->data, root->end, root->nchild);
+		for (i = 0; i < size; i++) {
+			print(root->keys[i], level + 1);
+		}
+	}
 }
 
-void delete (Trie *root, char *word) {
-  printf("\nImplementar a remocao\n");
-	if(search_auxiliary(root, word))
+void delete (Trie* root, char* word) {
+	if (!search_auxiliary(root, word)) {
+		printf("Essa chave nao existe\n");
+		return;
+	}
+	else
 	{
-		Trie *t= root;
+		Trie* t = root;
+		Trie* checkpoint  = root;
+		int check_index;
 		for(int i= 0;word[i]!='\0';i++)
 		{
-			int index=word[i]-'a';
-			if(t->keys[index]==NULL)//se nao houver mais de uma chave
-						//para de pegar o indice e no
-			t->nchild+=1;
-			t=t->keys[index];
-			if(t->end)
-				t[keys] = free_node;//libera ate o no
+			int index = word[i] - 'a';
+			t->nchild = t->nchild-1;
+			if ((t->nchild >= 1 && t->end)||(t->nchild >= 2 && !t->end)) {
+				checkpoint = t;
+				if(word[i+1]!='\0') //se nao for o final da palavra
+					check_index = (int)word[i+1] - 'a';
+			}
+			if (t->nchild > 0)
+				t->end = FALSE;
+			else{
+				free(checkpoint->keys[check_index]);
+				checkpoint->keys[check_index] = NULL;
+			}
 		}
-	
+	}	
 
-	}
-	else//faça nada
-}  
- 
+}
